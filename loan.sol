3032969e6feb3fd6258c23f4d9ed5dc333f0c18a
Leo Bogod <106600189+lleo-bogod-PRDCREATORS@users.noreply.github.com>
2022-06-20 09:43:06 +0000

    Update loan.sol

--- loan.sol
+++ loan.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 
 contract SmartProxy is Ownable {
     using SafeMath for uint256;
@@ -59,12 +60,21 @@ contract SmartProxy is Ownable {
     function setCollateralRate(uint256 _newRate) public onlyOwner {
         c_r = _newRate;
     }
-     function _getLatestPrice() public view returns (int256) {
-  // price of synthlp with oracle
+    function _getLatestPrice(address pairAddress, uint amount ) public view returns (int256) {
+     /* IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
+      * IERC20 token1 = IERC20(pair.token1);
+      *(uint Res0, uint Res1,) = pair.getReserves();
+     */
+    // decimals
+    uint res0 = Res0*(10**token1.decimals());
+    return((amount*res0)/Res1);
     }
     
     function getExchangeRate() public view returns (uint256) {
    // exchange rate 
+    }
+     function getBalance() public view returns (uint) {
+        return  SynthLp.balanceOf(msg.sender);
     }
 
     function setSynthLp(address _newAddress) public onlyOwner {
