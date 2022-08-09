021d653b95c2f3b33d1bf760b04762e3000aa6f0
klkvr <klkvrr@gmail.com>
2022-08-09 18:03:18 +0300

    added SynthV1.getAmounts())

--- contracts/idex/ethSynth/ETHSynthV1.sol
+++ contracts/idex/ethSynth/ETHSynthV1.sol
@@ -6,8 +6,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
+import "@prb/math/contracts/PRBMathUD60x18.sol";
 
 contract ETHSynthV1 is ERC20, Ownable {
+    using PRBMathUD60x18 for uint256;
+
     address public factory;
     uint256 public pid;
 
@@ -41,50 +44,8 @@ contract ETHSynthV1 is ERC20, Ownable {
     */
     function getAmounts() public view returns(uint256 token1Amount, uint256 token2Amount) {
         (uint256 token1Reserves, uint256 token2Reserves,) = pair.getReserves();
-        uint256 precision = 10 ** 10;
-        uint256 price = (token1Reserves * precision) / token2Reserves; // current price (multiplied by precision)
-        uint256 initialPrice = (token1InitialAmount * precision) / token2InitialAmount; // initial price (we need it to become same as current)
-        uint256 left;
-        uint256 right;
-        uint256 mid;
-        uint256 t1;
-        uint256 t2;
-        uint256 p;
-        if (initialPrice < price) { // binary search for determination of actual token1Amount and token2Amount
-            left = 0;               // Here we are looking for a value which we need to subtract from token1Amount or token2Amount
-            right = token2InitialAmount;
-            while (right - left > 1) {
-                mid = (right + left) / 2;
-                t2 = token2InitialAmount - mid;
-                t1 = (token1InitialAmount * token2InitialAmount) / t2;
-                p = (t1 * precision) / t2;
-                if (p < price) {
-                    left = mid;
-                }
-                else {
-                    right = mid;
-                }
-            }
-            token1Amount = t1;
-            token2Amount = t2;
-        } else {
-            left = 0;
-            right = token1InitialAmount;
-            while (right - left > 1) {
-                mid = (right + left) / 2;
-                t1 = token1InitialAmount - mid;
-                t2 = (token1InitialAmount * token2InitialAmount) / t1;
-                p = (t1 * precision) / t2;
-                if (p < price) {
-                    right = mid;
-                }
-                else {
-                    left = mid;
-                }
-            }
-            token1Amount = t1;
-            token2Amount = t2;
-        }
+        token2Amount = uint256((token1InitialAmount * token2InitialAmount * token2Reserves) / token1Reserves).sqrt();
+        token1Amount = uint256((token1InitialAmount * token2InitialAmount * token1Reserves) / token2Reserves).sqrt();
     }
     
     function getPrice() public view returns(uint256 price) {
