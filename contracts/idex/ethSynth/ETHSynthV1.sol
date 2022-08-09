7bd21f52e11f75a928bb6f465245bece382c5d98
klkvr <klkvrr@gmail.com>
2022-08-09 14:55:40 +0300

    added SynthV1.getAmounts())

--- contracts/idex/ethSynth/ETHSynthV1.sol
+++ contracts/idex/ethSynth/ETHSynthV1.sol
@@ -3,16 +3,83 @@ pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 contract ETHSynthV1 is ERC20, Ownable {
     address public factory;
     uint256 public pid;
 
+    IERC20 public token1;
+    IERC20 public token2;
+
+    IUniswapV2Pair private pair;
+
+    uint256 private token1InitialAmount;
+    uint256 private token2InitialAmount;
+
+
     /**
      * @dev Sets the values for {factory}.
      */
-    constructor() ERC20("ETHSynth", "SYNTH") {
+    constructor(IERC20 _token1, IERC20 _token2, uint256 _token1InitialAmount, uint256 _token2InitialAmount, IUniswapV2Pair _pair) ERC20("ETHSynth", "SYNTH") {
         factory = msg.sender;
+        token1 = _token1;
+        token2 = _token2;
+        token1InitialAmount = _token1InitialAmount;
+        token2InitialAmount = _token2InitialAmount;
+        pair = _pair;
+    }
+
+    /**
+     * @dev Returns the amounts of token1 and token2 which 1 Synth is representing at the moment
+    */
+    function getAmounts() public view returns(uint256 token1Amount, uint256 token2Amount) {
+        (uint256 token1Reserves, uint256 token2Reserves,) = pair.getReserves();
+        uint256 precision = 10 ** 10;
+        uint256 price = (token1Reserves * precision) / token2Reserves; // current price (multiplied by precision)
+        uint256 initialPrice = (token1InitialAmount * precision) / token2InitialAmount;
+        uint256 left;
+        uint256 right;
+        uint256 mid;
+        uint256 t1;
+        uint256 t2;
+        uint256 p;
+        if (initialPrice < price) {
+            left = 0;
+            right = token2InitialAmount;
+            while (right - left > 1) {
+                mid = (right + left) / 2;
+                t2 = token2InitialAmount - mid;
+                t1 = (token1InitialAmount * token2InitialAmount) / t2;
+                p = (t1 * precision) / t2;
+                if (p < price) {
+                    left = mid;
+                }
+                else {
+                    right = mid;
+                }
+            }
+            token1Amount = t1;
+            token2Amount = t2;
+        } else {
+            left = 0;
+            right = token1InitialAmount;
+            while (right - left > 1) {
+                mid = (right + left) / 2;
+                t1 = token1InitialAmount - mid;
+                t2 = (token1InitialAmount * token2InitialAmount) / t1;
+                p = (t1 * precision) / t2;
+                if (p < price) {
+                    right = mid;
+                }
+                else {
+                    left = mid;
+                }
+            }
+            token1Amount = t1;
+            token2Amount = t2;
+        }
     }
 
     /**
