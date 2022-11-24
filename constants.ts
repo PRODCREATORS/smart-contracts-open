e3b66aef3c3804eb908a9d251410a60390927b30
atimofeev <atimofeev@ent-dx.com>
2022-11-24 21:36:36 +0300

    Tracing nameTags

--- constants.ts
+++ constants.ts
@@ -1,6 +1,21 @@
 // ONLY FOR ETH
-export const UNSIWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-export const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+
+
+export const USDT                                 = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+export const WBTC                                 = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+export const WETH                                 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+export const BaseRewardPool                       = "0x9D5C5E364D81DaB193b72db9E9BE9D8ee669B652";
+export const ConverxBooster                       = "0xF403C135812408BFbE8713b5A23a04b3D48AAE31";
+export const UNSIWAP_ROUTER                       = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+export const _3CryptoV2Pool                       = "0xD51a44d3FaE010294C616388b506AcdA1bfAAE46";
+export const crv3crypto__gauge                    = "0xDeFd8FdD20e0f34115C7018CCfb655796F6B2168";
+export const Curvefi_USD__BTC__ETH                = "0xc4AD29ba4B3c580e6D59105FFf484999997675Ff";
+export const UniswapPairV2WBTCWETC                = "0xBb2b8038a1640196FbE3e38816F3e67Cba72D940";
+export const UniswapPairV2WETHUSDT                = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852";
+export const Curvefi_USD__BTC__ETH_Convex_Deposit = "0x903C9974aAA431A765e60bC07aF45f0A1B3b61fb";
+export const Curvefi_Pool_Math                    = "0x8f68f4810cce3194b6cb6f3d50fa58c2c9bdd1d5";
+export const Curvefi_Front_Utils                  = "0x40745803C2faA8E8402E2Ae935933D07cA8f355c";
+
 export const WETH_ABI = [
   {
     anonymous: false,
