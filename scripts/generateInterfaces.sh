dd22fa6579570fbac8ae50d5e8a6b6de6f717452
atimofeev <atimofeev@ent-dx.com>
2022-11-24 22:40:23 +0300

    Update interface generator script + ILiquidityGaugeV3

--- scripts/generateInterfaces.sh
+++ scripts/generateInterfaces.sh
@@ -1,8 +1,22 @@
 #! /bin/bash
 set -e
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
-echo "Download tricypto pool abi (address: 0xd51a44d3fae010294c616388b506acda1bfaae46)..."
-curl http://api.etherscan.io/api\?module\=contract\&action\=getabi\&address\=0xd51a44d3fae010294c616388b506acda1bfaae46\&format\=raw -H "User-Agent: Chrome" > /tmp/tricrypto_pool_abi.json
-echo "Genrating interface..."
-cat /tmp/tricrypto_pool_abi.json | npx abi-to-sol ITricryptoPool > $PROJECT_ROOT/contracts/interfaces/thirdparty/ITricryptoPool.sol
-echo "Done."
+
+yank () {
+  curl -s "http://api.etherscan.io/api?module=contract&action=getabi&address=$1&format=raw" -H "User-Agent: Chrome" > /tmp/$2
+}
+
+gen () {
+  sleep 5
+  echo "Download $2 abi (address: $1)..."
+  yank $1 $2_abi.json
+  echo "Genrating interface..."
+  cat /tmp/$2_abi.json | npx abi-to-sol $2 > $PROJECT_ROOT/contracts/interfaces/thirdparty/$2.sol
+  echo "Done."
+  echo ""
+}
+
+gen "0xd51a44d3fae010294c616388b506acda1bfaae46" "ITricryptoPool" 
+gen "0xdefd8fdd20e0f34115c7018ccfb655796f6b2168" "ILiquidityGaugeV3" 
+
+echo "All done."
