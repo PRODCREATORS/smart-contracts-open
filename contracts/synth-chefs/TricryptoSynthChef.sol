753f3034e08a21c091fa648b4d66954e9dd5e59f
atimofeev <atimofeev@ent-dx.com>
2022-11-25 05:44:55 +0300

    Basically working; Needs more tests

--- contracts/synth-chefs/TricryptoSynthChef.sol
+++ contracts/synth-chefs/TricryptoSynthChef.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../interfaces/thirdparty/ITricryptoPool.sol";
+import "../interfaces/thirdparty/ILiquidityGaugeV3.sol";
 import "./BaseSynthChef.sol";
 import "hardhat/console.sol";
 
@@ -10,11 +11,13 @@ contract TricryptoSynthChef is BaseSynthChef {
   using SafeERC20 for IERC20;
 
   ITricryptoPool pool;
+  ILiquidityGaugeV3 gauge;
   mapping(address => uint8) token2PoolIndex;
   uint8 N_TOKENS = 3;
-
+  
   constructor(
-    ITricryptoPool _convex,
+    ITricryptoPool _pool,
+    ILiquidityGaugeV3 _gauge,
     address _DEXWrapper,
     address _stablecoin,
     address[] memory _rewardTokens,
@@ -23,9 +26,94 @@ contract TricryptoSynthChef is BaseSynthChef {
   )
     BaseSynthChef(_DEXWrapper, _stablecoin, _rewardTokens, _fee, _feeCollector)
   {
-    pool = _convex;
+    pool = _pool;
+    gauge = _gauge;
     _SetupTokenIndices();
     _SetupAllowances();
+    _SetupGaugeAllowance();
+  }
+
+
+  event ExpectLPs(uint256 amount);
+
+  function lpToken() public view returns (address) { return pool.token(); }
+  function getGauge() public view returns (ILiquidityGaugeV3) { return gauge; }
+
+  function _depositToFarm(uint256 _pid, uint256 _amount) internal override { gauge.deposit(_amount);}
+
+  function _harvest(uint256 _pid) internal override { gauge.claim_rewards(); }
+
+  function _withdrawFromFarm(uint256 _pid, uint256 _amount) internal override { gauge.withdraw(_amount, true); }
+
+  function getLPAmountOnFarm(uint256 _pid) public view override returns (uint256) { return gauge.balanceOf(address(this)); }
+
+  function _SetupGaugeAllowance() internal { IERC20(lpToken()).safeIncreaseAllowance(address(gauge), type(uint256).max); }
+
+  /**
+   * NOTE: Currently only single pool is implemented;
+   *       Instead of depoiting and combination of 3 tokens,
+   *       our api basically allows adding only one token at a time; 
+   *       maybe we should split input like: 
+   *        -  Token From: 98%
+   *        -  Rest: 2%
+   * 
+   *       Also returned amount of LP Tokens minted is not guranieeed
+   *       to be correct, because `pool.add_liqidity` method does not
+   *       return the ammmount of LP Tokens minted; We base our
+   *       expectaions on the recived amount of LP Tokens from the 
+   *       `pool.calc_token_amount` call which does not account for fees
+   *       So... trying to be on the safe side we subtract the fees from 
+   *       the result
+   */
+  function _addLiquidity(uint256 _pid, address _tokenFrom, uint256 _amount) internal override returns (uint256 LPAmount) {
+
+    uint8 idx = token2PoolIndex[_tokenFrom];
+
+    uint256[3] memory amounts = [uint(0), uint(0), uint(0)];
+    amounts[idx] = _amount;
+
+    uint256[3] memory xp = _getPoolBalances();
+    uint256 fees = pool.calc_token_fee(amounts, xp);
+
+    uint256 expected = pool.calc_token_amount(amounts, true); // This does not account for fees...
+
+    pool.add_liquidity(amounts, expected); // Should we adjust our expectatins since it does not account for fees?
+    // Emit the expected ammount of lp tokens to test is our assumptions correct
+    emit ExpectLPs(expected - fees);
+    return expected - fees;
+  }
+
+  function _removeLiquidity(uint256 _pid, uint256 _amount) internal override returns (TokenAmount[] memory) {
+
+    TokenAmount[] memory tokenAmounts = new TokenAmount[](3);
+    
+    uint256[3] memory min_amounts = [uint(0), uint(0), uint(0)];
+    uint256 totalSupply           = IERC20(lpToken()).totalSupply();
+    
+    address[3] memory tokens   = _getPoolTokens();
+    uint256[3] memory balances = _getPoolBalances();
+
+    for(uint8 i = 0; i < N_TOKENS; ++i) {
+      min_amounts[i] = ((balances[i] * _amount) / totalSupply) - 1 ;
+      tokenAmounts[i] = TokenAmount(min_amounts[i], tokens[i]);
+    }
+
+    pool.remove_liquidity(_amount, min_amounts);
+    return tokenAmounts;
+  }
+
+
+  function _getTokensInLP(uint256 _pid) internal view override returns (TokenAmount[] memory) {
+    TokenAmount[] memory tokens = new TokenAmount[](3);
+
+    address[3] memory _tokens  = _getPoolTokens();
+    uint256[3] memory reserves = _getPoolBalances();
+
+    for(uint8 i = 0; i < N_TOKENS; ++i) {
+      tokens[i] = TokenAmount({amount: reserves[i], token: _tokens[i]});
+    }
+
+    return tokens;
   }
 
   function _SetupTokenIndices() internal {
@@ -52,67 +140,13 @@ contract TricryptoSynthChef is BaseSynthChef {
     return balances; 
   }
 
-  function lpToken() public view returns (address) {
-    return pool.token();
+  function _getPoolTokens() internal view returns (address[3] memory) {
+    assert(N_TOKENS == 3); // Just to be safe
+    address[3] memory tokens;
+    for (uint8 i = 0; i < N_TOKENS; ++i) {
+      tokens[i] = pool.coins(i);
+    }
+    return tokens; 
   }
 
-  function _harvest(uint256 _pid) internal override {}
-
-  function _withdrawFromFarm(uint256 _pid, uint256 _amount) internal override {}
-
-  // Deposit singe token
-  function _depositToFarm(uint256 _pid, uint256 _amount) internal override {}
-
-  function _removeLiquidity(
-    uint256 _pid,
-    uint256 _amount
-  ) internal override returns (TokenAmount[] memory tokenAmounts) {}
-
-  /**
-   * NOTE: Currently only single pool is implemented;
-   *       Instead of depoiting and combination of 3 tokens,
-   *       our api basically allows adding only one token at a time; 
-   *       maybe we should split input like: 
-   *        -  Token From: 98%
-   *        -  Rest: 2%
-   * 
-   *       Also returned amount of LP Tokens minted is not guranieeed
-   *       to be correct, because `pool.add_liqidity` method does not
-   *       return the ammmount of LP Tokens minted; We base our
-   *       expectaions on the recived amount of LP Tokens from the 
-   *       `pool.calc_token_amount` call which does not account for fees
-   *       So... trying to be on the safe side we subtract the fees from 
-   *       the result
-   */
-  function _addLiquidity(
-    uint256 _pid,
-    address _tokenFrom,
-    uint256 _amount
-  ) internal override returns (uint256 LPAmount) {
-
-    uint8 idx = token2PoolIndex[_tokenFrom];
-
-    uint256[3] memory amounts = [uint(0), uint(0), uint(0)];
-    amounts[idx] = _amount;
-
-    uint256[3] memory xp = _getPoolBalances();
-    uint256 fees = pool.calc_token_fee(amounts, xp);
-
-    uint256 expected = pool.calc_token_amount(amounts, true); // This does not account for fees...
-
-    pool.add_liquidity(amounts, expected); // Should we adjust our expectatins since it does not account for fees?
-    
-    emit ExpectLPs(expected - fees); // Mainly for testing
-    return expected - fees; // I Guess?
-  }
-
-  event ExpectLPs(uint256 amount);
-
-  function _getTokensInLP(
-    uint256 _pid
-  ) internal view override returns (TokenAmount[] memory tokens) {}
-
-  function getLPAmountOnFarm(
-    uint256 _pid
-  ) public view override returns (uint256 amount) {}
 }
--- test/eth3.ts
+++ test/eth3.ts
@@ -1,12 +1,7 @@
-import { expect } from "chai";
+import { assert, expect } from "chai";
 import { BigNumber } from "ethers";
 import hre, { ethers } from "hardhat";
 import * as mainnet from "../constants";
-import { ERC20, IWETH } from "../typechain-types";
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-type WETH = ERC20 & IWETH;
-
-const GAUGE = "0xdefd8fdd20e0f34115c7018ccfb655796f6b2168"; // For real was a pain in the ass to verify
 
 async function trace<T>(fn: () => Promise<T>): Promise<T> {
   hre.tracer.enabled = !!process.env.TRACE;
@@ -15,7 +10,7 @@ async function trace<T>(fn: () => Promise<T>): Promise<T> {
   return out;
 }
 
-describe("ETH Synth Chef", function () {
+describe("ETH Synth Chef", async function () {
   async function chefFixture() {
     const [owner] = await ethers.getSigners();
 
@@ -24,7 +19,7 @@ describe("ETH Synth Chef", function () {
     );
     const ChefFactory = await ethers.getContractFactory("TricryptoSynthChef");
 
-    const wETH = (await ethers.getContractAt("IWETH", mainnet.WETH)) as WETH;
+    const wETH = await ethers.getContractAt("WETH", mainnet.WETH);
     wETH.deposit({ value: ethers.utils.parseEther("1") });
 
     const UniswapWrapper = await UniswapWrapperFactory.deploy(
@@ -35,6 +30,7 @@ describe("ETH Synth Chef", function () {
 
     const chef = await ChefFactory.deploy(
       mainnet._3CryptoV2Pool,
+      mainnet.crv3crypto__gauge,
       UniswapWrapper.address,
       mainnet.USDC,
       [mainnet.CRV],
@@ -48,53 +44,86 @@ describe("ETH Synth Chef", function () {
     return { wETH, chef, owner };
   }
 
-  it("Deposit (cold)", async function () {
-    const { wETH, chef } = await loadFixture(chefFixture);
+  describe("Basic functions", async () => {
+    let { chef, wETH, owner } = {} as Awaited<ReturnType<typeof chefFixture>>;
 
-    const lpAddr = await chef.lpToken();
-    const LP = await ethers.getContractAt("ERC20", lpAddr);
+    it("Deploys", async () => {
+      const fixture = await chefFixture();
+      chef = fixture.chef;
+      wETH = fixture.wETH;
+      owner = fixture.owner;
+    });
 
-    let balance = await LP.balanceOf(chef.address);
-    expect(balance.eq(0));
+    it("Deposit", async function () {
+      const lpAddr = await chef.lpToken();
+      const LP = await ethers.getContractAt("ERC20", lpAddr);
 
-    await wETH.approve(chef.address, ethers.constants.MaxUint256);
+      let balance = await LP.balanceOf(chef.address);
+      expect(balance.eq(0)).to.be.true;
 
-    const tx = await trace(() =>
-      chef.deposit(0, wETH.address, ethers.utils.parseEther("1.0"), 0, {
-        gasLimit: 2500000,
-      })
-    );
+      await wETH.approve(chef.address, ethers.constants.MaxUint256);
 
-    const receipt = await tx.wait(1);
-    const event = chef.interface.parseLog(receipt.logs!.at(-2)!);
-    const expectedAmount = event.args[0] as BigNumber;
+      const tx = await trace(() =>
+        chef.deposit(0, wETH.address, ethers.utils.parseEther("1.0"), 0, {
+          gasLimit: 2500000,
+        })
+      );
 
-    balance = await LP.balanceOf(chef.address);
-    expect(balance.eq(expectedAmount));
+      const receipt = await tx.wait(1);
+      const event = receipt.events?.find(e => e.event === 'ExpectLPs');
+      assert(event && event.args);
+      
+      const expectedAmount = event.args[0] as BigNumber;
+
+      balance = await LP.balanceOf(chef.address);
+      console.log(expectedAmount, balance);
+      expect(balance.eq(expectedAmount));
+    });
+
+    it("getBalanceOnFarm", async function () {
+      const bal = await trace(() => chef.getBalanceOnFarm(0));
+      console.log(bal);
+    });
+
+    // it("Compound", async function () {
+    //   let balanceBeforeCompound = await chef.getBalanceOnFarm(0);
+    //   await ethers.provider.send("evm_increaseTime", [3600 * 24 * 365]);
+    //   const tx = await chef.compound(0, { gasLimit: 2500000 });
+    //   await tx.wait(1);
+    //   let balanceAfterCompound = await chef.getBalanceOnFarm(0);
+    //   console.log({ balanceBeforeCompound, balanceAfterCompound })
+    //   //expect(balanceAfterCompound).to.be.greaterThan(balanceBeforeCompound);
+    // });
+    it("User checkpoint", async function () {
+     const gague = await chef.getGauge();
+     const g = await ethers.getContractAt('ILiquidityGaugeV3', gague);
+
+     await trace(() => g.user_checkpoint(owner.address));
+
+    })
+    it("Withdraw", async function () {
+      let balanceBeforeWithdraw = await chef.getBalanceOnFarm(0);
+      //await ethers.provider.send("evm_increaseTime", [3600 * 24 * 365]);
+      let amount = await chef.getLPAmountOnFarm(0);
+          amount = amount.div(10);
+
+      
+      console.log("Amount to withdraw",  amount.lt("25613014622422005801572"));
+      hre.tracer.enabled = true;
+      const tx = await chef.withdraw(
+        0,
+        "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        amount,
+        owner.getAddress(),
+        0,
+      ); // TODO: FAILS AT get_relative_weight <---- WRONG!
+      // We are just withdrawing more than we have
+      console.log(tx.hash)
+      await tx.wait(1)
+      console.log(tx.hash);
+      let balanceAfterWithdraw = await chef.getBalanceOnFarm(0);
+      console.log({ balanceBeforeWithdraw, balanceAfterWithdraw})
+      //expect(balanceAfterWithdraw).to.be.lessThan(balanceBeforeWithdraw);
+    });
   });
-
-  // it("Compound", async function () {
-  //   let balanceBeforeCompound = await chef.getBalanceOnFarm(0);
-  //   await ethers.provider.send("evm_increaseTime", [3600 * 24 * 365]);
-  //   const tx = await chef.compound(0, { gasLimit: 2500000 });
-  //   await tx.wait(1);
-  //   let balanceAfterCompound = await chef.getBalanceOnFarm(0);
-  //   console.log({ balanceBeforeCompound, balanceAfterCompound })
-  //   //expect(balanceAfterCompound).to.be.greaterThan(balanceBeforeCompound);
-  // });
-
-  // it("Withdraw", async function () {
-  //   let balanceBeforeWithdraw = await chef.getBalanceOnFarm(0);
-  //   const tx = await chef.withdraw(
-  //     0,
-  //     "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-  //     (await chef.getLPAmountOnFarm(0)).div(10),
-  //     owner.getAddress(),
-  //     0, { gasLimit: 2500000 }
-  //   );
-  //   await tx.wait(1)
-  //   let balanceAfterWithdraw = await chef.getBalanceOnFarm(0);
-  //   console.log({ balanceBeforeWithdraw, balanceAfterWithdraw})
-  //   //expect(balanceAfterWithdraw).to.be.lessThan(balanceBeforeWithdraw);
-  // });
 });
